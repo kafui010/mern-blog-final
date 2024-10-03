4748a59367724b9c5730c27ba7d1831122a03696
Kafui <kafuibright4@gmail.com>
2024-10-03 19:06:13 +0000

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';  // Import dotenv
+import userRoutes from './routes/user.route.js';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -20,3 +21,6 @@ const app = express();
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
 });
+
+
+app.use('/api/user', userRoutes);
--- api/models/user.nodel.js
+++ api/models/user.nodel.js
@@ -15,11 +15,11 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-    }, 
-    
-}, { timestamps: true }
+    },
 
+},{ timestamps: true }
 );
 
 const User = mongoose.model('User', userSchema);
+
 export default User;
